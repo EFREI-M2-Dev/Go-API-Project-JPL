e79f93e6f0e8a3b9b656f0d21fe70359c8f76405
Suylo <spoownz@yahoo.com>
2025-06-27 11:20:10 +0200

    fix(models): correct Shortcode field name to ShortCode in link.go

--- internal/models/link.go
+++ internal/models/link.go
@@ -3,7 +3,7 @@ package models
 // Link /**
 type Link struct {
 	ID        uint   `gorm:"primaryKey"`
-	Shortcode string `gorm:"uniqueIndex;unique;size:10;not null"`
+	ShortCode string `gorm:"uniqueIndex;unique;size:10;not null"`
 	LongURL   string `gorm:"not null"`
 	CreatedAt string `gorm:"autoCreateTime;not null"`
 }
