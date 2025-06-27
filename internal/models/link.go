3ea555f64dc19a4fcc37f9a6090f0e9f0cfc5135
Suylo <spoownz@yahoo.com>
2025-06-27 11:18:58 +0200

    fix(models): update link.go struct fields to export

--- internal/models/link.go
+++ internal/models/link.go
@@ -3,7 +3,7 @@ package models
 // Link /**
 type Link struct {
 	ID        uint   `gorm:"primaryKey"`
-	shortcode string `gorm:"uniqueIndex;unique;size:10;not null"`
-	longURL   string `gorm:"not null"`
-	createdAt string `gorm:"autoCreateTime;not null"`
+	Shortcode string `gorm:"uniqueIndex;unique;size:10;not null"`
+	LongURL   string `gorm:"not null"`
+	CreatedAt string `gorm:"autoCreateTime;not null"`
 }
