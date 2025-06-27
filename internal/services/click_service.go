357bddeefde49cef35b933e64f32cd63a7cc34fc
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-06-27 11:40:17 +0200

    feat(click): Click WIP ClickRecord working

--- internal/services/click_service.go
+++ internal/services/click_service.go
@@ -7,9 +7,12 @@ import (
 	"github.com/axellelanca/urlshortener/internal/repository" // Importe le package repository
 )
 
-// TODO : créer la struct
+// DONE : créer la struct
 // ClickService est une structure qui fournit des méthodes pour la logique métier des clics.
 // Elle est juste composer de clickRepo qui est de type ClickRepository
+type ClickService struct {
+	clickRepo repository.ClickRepository
+}
 
 // NewClickService crée et retourne une nouvelle instance de ClickService.
 // C'est la fonction recommandée pour obtenir un service, assurant que toutes ses dépendances sont injectées.
@@ -22,9 +25,10 @@ func NewClickService(clickRepo repository.ClickRepository) *ClickService {
 // RecordClick enregistre un nouvel événement de clic dans la base de données.
 // Cette méthode est appelée par le worker asynchrone.
 func (s *ClickService) RecordClick(click *models.Click) error {
-	// TODO 1: Appeler le ClickRepository (CreateClick) pour créer l'enregistrement de clic.
-	// Gérer toute erreur provenant du repository.
-
+	if err := s.clickRepo.CreateClick(click); err != nil {
+		return fmt.Errorf("[Service::CreateClick] %w", err)
+	}
+	return nil
 }
 
 // GetClicksCountByLinkID récupère le nombre total de clics pour un LinkID donné.
